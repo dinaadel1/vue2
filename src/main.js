bbc9ffc4551bc3178bb69a250da7efb314716c68
Dina Adel <cc201029@fhstp.ac.at>
2021-11-04 17:59:26 +0100

    pokedex

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory} from
+        'vue-router';
 
-createApp(App).mount('#app')
+import Home from './pages/Home.vue'
+import Calculator from './pages/Calculator.vue'
+import Pokedex from './pages/Pokedex.vue'
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/Pokedex', component: Pokedex },
+    { path: '/Calculator', component: Calculator },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+
+
+});
+
+const app = createApp({});
+app.use(router)
+app.mount('#app');
+
+
---
+++ src/pages/Pokedex.vue
@@ -0,0 +1,42 @@
+<script>
+const POKEMON_API="https://pokeapi.co/api/v2/pokemon?limit=151"
+const POKEMON_IMAGE="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+
+export default {
+  name: 'PokedexPage',
+  data() {
+    return {
+      pokemon: []
+    };
+  },
+
+  methods: {
+    async catchAll() {
+      const result = await fetch(POKEMON_API);
+      const data = await result.json();
+      this.pokemon = data.results;
+    },
+  },
+};
+</script>
+
+
+
+<template>
+  <h1> Pokedex </h1>
+
+  <button @click="catchAll">Catch Pokemon</button>
+
+  {{ pokemon }}
+
+  <ul>
+    <li v-for="pokeman in pokemon">
+      <p>{{ pokeman.name }}</p>
+      <img
+          :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png`"
+           :alt="name"
+      />
+    </li>
+  </ul>
+
+</template>
